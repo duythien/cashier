30e8b8f91fa2cf783850e7217ced9afd2b70fe59
Thien <fcduythien@gmail.com>
2016-08-29 21:52:10 +0700

    Remove some text

--- tests/service.phalcon.php
+++ tests/service.phalcon.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * 5App : PharmaScout software
  *
  * Licensed under The GNU License
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @link    http://dev.5App.com.gt 5App Project
+ * @link    http://phanbook.com Project
  * @since   1.0.0
  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
  */
